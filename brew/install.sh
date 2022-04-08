adad9935ef5cb2cb318a276e4cf867a7d7cdf3f9
Ryan Wessel <rwessel@dataminr.com>
2022-04-08 16:21:54 -0400

    Add ffmpeg and imagemagick to homebrew

--- brew/install.sh
+++ brew/install.sh
@@ -9,7 +9,7 @@ function install_homebrew_dependencies() {
 
   brew tap homebrew/cask-fonts || true
   brew install --quiet --cask authy rectangle visual-studio-code spotify android-studio notion hyper slack font-ubuntu-mono-nerd-font || true
-  brew install --quiet vim git nodenv rbenv jenv react-native-debugger starship jq bat lsd fd hyperfine delta yarn || true
+  brew install --quiet vim git nodenv rbenv jenv react-native-debugger starship jq bat lsd fd hyperfine delta yarn ffmpeg imagemagick || true
 }
 
 install_homebrew_dependencies
