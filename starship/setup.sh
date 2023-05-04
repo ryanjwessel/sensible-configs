6c62d79f07bb6e731d05df128ae14b1dac7f0622
Ryan Wessel <rwessel@dataminr.com>
2023-05-04 15:19:51 -0400

    Add 1s default command timeout to Starship configuration

--- starship/setup.sh
+++ starship/setup.sh
@@ -1,3 +1,4 @@
 starship preset plain-text-symbols -o ~/.config/starship.toml
+echo "command_timeout = 1000\n\n$(cat ~/.config/starship.toml)" > ~/.config/starship.toml
 
 echo "Configured Starship."
