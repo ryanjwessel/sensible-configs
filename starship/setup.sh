55dca767a588befc430551292f90ada616427a10
Ryan Wessel <ryan@wesselstudios.com>
2025-02-10 17:27:31 -0500

    Handle case when starship config file does not exist yet on system

--- starship/setup.sh
+++ starship/setup.sh
@@ -1,3 +1,5 @@
+mkdir -p ~/.config
+touch ~/.config/starship.toml
 starship preset plain-text-symbols -o ~/.config/starship.toml
 echo "command_timeout = 1000\n\n$(cat ~/.config/starship.toml)" > ~/.config/starship.toml
 
