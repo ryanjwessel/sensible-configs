32f83db503efb38a73b60e925579f4375194c1bd
Ryan Wessel <ryanwessel@Ryans-MacBook-Pro.local>
2025-02-10 17:23:19 -0500

    Add Warp terminal to default homebrew applications to install

--- brew/setup.sh
+++ brew/setup.sh
@@ -4,7 +4,7 @@ if ! which brew &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-brew install --quiet --cask android-studio copilot font-ubuntu-mono-nerd-font notion postman raycast rectangle slack spotify visual-studio-code || true
+brew install --quiet --cask android-studio copilot font-ubuntu-mono-nerd-font notion postman raycast rectangle slack spotify visual-studio-code warp || true
 brew install --quiet git git-delta jenv jq lsd nodenv python rbenv starship vim yarn || true
 
 echo "Installed Homebrew dependencies."
