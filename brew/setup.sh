9be48753327be5feb1277d42d94f87c78ee72f9e
Ryan Wessel <ryan@wesselstudios.com>
2025-02-18 10:07:07 -0500

    Alphabetize dependencies installed by brew

--- brew/setup.sh
+++ brew/setup.sh
@@ -4,7 +4,37 @@ if ! which brew &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-brew install --quiet --cask android-studio copilot cursor font-ubuntu-mono-nerd-font notion postman raycast rectangle readdle-spark slack spotify visual-studio-code warp || true
-brew install --quiet git git-delta jenv jq lsd nodenv python rbenv starship vim yarn || true
+cask_dependencies=(
+  android-studio
+  copilot
+  cursor
+  font-ubuntu-mono-nerd-font
+  notion
+  postman
+  raycast
+  readdle-spark
+  rectangle
+  slack
+  spotify
+  visual-studio-code
+  warp
+)
+
+dependencies=(
+  git
+  git-delta
+  jenv
+  jq
+  lsd
+  nodenv
+  python
+  rbenv
+  starship
+  vim
+  yarn
+)
+
+brew install --quiet --cask "${cask_dependencies[@]}" || true
+brew install --quiet "${dependencies[@]}" || true
 
 echo "Installed Homebrew dependencies."
